29ceb06f88b2487de2d317f8c63880c60690916e
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-26 19:18:24 +0800

    header

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,19 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Text
 } from 'react-native';
 
-export default class NUSFindRoom extends Component {
-  render() {
+import Header from './src/components/header.js'
+
+const App = () => {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <Header headerText={"NUS VenueFinder"}/>
     );
-  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('NUSFindRoom', () => NUSFindRoom);
+AppRegistry.registerComponent('NUSFindRoom', () => App);
