f524f77cf2b60b6081b645fa6f8262db5a6eda7c
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-27 08:56:24 +0800

    no api calling

---
+++ src/components/venues.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Text, View} from 'react-native'
+
+
+class Venues extends Component{
+	render(){
+		return (
+			<View>
+
+	        	{<Text>Venue 1</Text>}
+
+	    	</View>
+    	);
+	}
+
+
+}
+
+export default Venues;
+
