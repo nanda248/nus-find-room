b2c6c31e9868ebb8f827b1a790883955f1dada4b
tengkuhafidz <tengkuhafidz@gmail.com>
2017-01-29 13:36:40 +0800

    ListView

--- src/components/venueSingle.js
+++ src/components/venueSingle.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, TouchableHighlight} from 'react-native'
 
 const VenueSingle = (props) => {
 	return (
-		<View >
-        	<Text>
-          		{props.venue.roomname} | {props.venue.dept}
-        	</Text>
-    	</View>
+		<TouchableHighlight underlayColor="#dddddd" style={{height: 50}}>
+			<View >
+	        	<Text style={{fontSize: 15, color: "#333333"}} numberOfLines={1}>
+	          		{props.venue.roomname} 
+	        	</Text>
+	        	<Text style={{fontSize: 10, color: "#dddddd"}} numberOfLines={1}>
+	          	 	{props.venue.dept}
+	        	</Text>
+	        	<View style={{height: 1, backgroundColor: "#dddddd"}} />
+	    	</View>
+	    </TouchableHighlight>
     );
 }
 
--- src/components/venues.js
+++ src/components/venues.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, ListView} from 'react-native'
 
 import VenueSingle from './venueSingle.js'
 
 
 class Venues extends Component{
 
-	state = {venues: []}
+	constructor() {
+	    super();
+	    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+	    this.state = {
+	      venues: ds.cloneWithRows([]),
+	    };
+	 } 
 
 	componentWillMount(){
 	    fetch('http://api.nusmods.com/venuesRaw.json')
 	      .then((response) => response.json())
 	      .then((responseJson) => {
-	        this.setState({venues: responseJson})
+	        this.setState({venues: this.state.venues.cloneWithRows(responseJson)})
 	      })
 	      .catch((error) => {
 	        console.error(error);
@@ -26,9 +32,10 @@ class Venues extends Component{
 	render(){
 		console.log('try')
 		return (
-			<View>
-				{this.renderVenues()}
-	    	</View>
+			<ListView
+		        dataSource={this.state.venues}
+		        renderRow={(venue) => <VenueSingle key={venue.roomcode} venue={venue}/>}
+		    />
     	);
 	}
 
